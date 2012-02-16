6d4e2f5b460eac174fe989f8aaa573829f93d7a8
Kumaresan Rajeswaran <kumaresan.myl@gmail.com>
2012-02-17 00:41:30 +0530

    finishing part1 of guide

--- apps/kadhir/kadhir.js
+++ apps/kadhir/kadhir.js
@@ -7,38 +7,74 @@
 Kadhir = SC.Application.create();
 
 Kadhir.Task = SC.Object.extend({
-      title: null,
-      isDone: false
-});
- 
-Kadhir.CreateTaskView = SC.TextField.extend({
-    insertNewline: function() {
-    var value = this.get('value');
- 
-    if (value) {
-      Kadhir.taskListController.createTask(value);
-      this.set('value', '');
-    }
-  }
+    title: null,
+    isDone: false
 });
 
+Kadhir.CreateTaskView = SC.TextField.extend({
+    insertNewline: function() {
+                       var value = this.get('value');
+
+                       if (value) {
+                           Kadhir.taskListController.createTask(value);
+                           this.set('value', '');
+                       }
+                   }
+});
+
+
+Kadhir.MarkDoneView = SC.Checkbox.extend({
+    titleBinding: '.parentView.content.title',
+    valueBinding: '.parentView.content.isDone'
+});
+
+
+Kadhir.StatsView = SC.TemplateView.extend({
+    remainingBinding: 'Kadhir.taskListController.remaining',
+
+    displayRemaining: function() {
+        var remaining = this.get('remaining');
+        return remaining + (remaining === 1 ? " item" : " items");
+    }.property('remaining')
+});
+
+
 SC.ready(function() {
-  Kadhir.mainPane = SC.TemplatePane.append({
-    layerId: 'kadhir',
-    templateName: 'kadhir'
-  });
+    Kadhir.mainPane = SC.TemplatePane.append({
+        layerId: 'kadhir',
+        templateName: 'kadhir'
+    });
 });
 
 Kadhir.taskListController = SC.ArrayController.create({
-   // Initialize the array controller with an empty array.
-   content: [],
 
-  // Creates a new todo with the passed title, then adds it
-  // to the array.
-  createTask: function(title) {
-    var task = Kadhir.Task.create({ title: title });
-    this.pushObject(task);
-  }
+    // Initialize the array controller with an empty array.
+    content: [],
+
+    // Creates a new todo with the passed title, then adds it
+    // to the array.
+    createTask: function(title) {
+        var task = Kadhir.Task.create({ title: title });
+        this.pushObject(task);
+    },
+
+    remaining: function() {
+        return this.filterProperty('isDone', false).get('length');
+    }.property('@each.isDone'),
+
+    clearCompletedTodos: function() {
+        this.filterProperty('isDone', true).forEach(this.removeObject, this);
+    },
+    
+    allAreDone: function(key, value) {
+        if (value !== undefined) {
+            this.setEach('isDone', value);
+            return value;
+        } else {
+            return this.get('length') && this.everyProperty('isDone', true);
+        }
+    }.property('@each.isDone')
+
 });
 
 
